577d0329fcaf2e71267eebaeb698727239055613
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-09-05 14:42:42 -0400

    Linux compatibility

--- test/cli.test.js
+++ test/cli.test.js
@@ -3,9 +3,12 @@ const { join } = require("path");
 const { getChildProcessHelpers } = require("./util/getChildProcessHelpers");
 
 const dockerNodeImage = "node:10.22.0-alpine3.9";
+const isWindows = process.platform === "win32";
 
 describe("cli", () => {
   it("child exit (don't kill container)", async () => {
+    // TODO: Why does this test take so long, especially in Github Actions?
+    jest.setTimeout(10000);
     const runArgs = ["--rm", dockerNodeImage, "-e", "console.log('hello')"];
     const cp = spawnBin(["--name", "hello", ...runArgs]);
     const cph = getChildProcessHelpers(cp);
@@ -18,14 +21,18 @@ describe("cli", () => {
     ]);
     await cph.childExited;
     expect(cp.exitCode).toBe(0);
-    expect(cp.signalCode).toBeNull();
   });
   it("signal exit (kill container)", async () => {
-    const killArgs = ["--signal", "SIGTERM"];
     const script =
       "console.log('hello');process.on('SIGTERM',()=>{console.log('goodbye');process.exit();});setInterval(()=>{},1000)";
     const runArgs = ["--rm", dockerNodeImage, "-e", script];
-    const cp = spawnBin(["--name", "hello-goodbye", ...killArgs, ...runArgs]);
+    const cp = spawnBin([
+      "--name",
+      "hello-goodbye",
+      "--signal",
+      "SIGTERM",
+      ...runArgs
+    ]);
     const cph = getChildProcessHelpers(cp);
     await cph.when(line => line === "hello");
     expect(cph.readOutput()).toStrictEqual([
@@ -36,23 +43,22 @@ describe("cli", () => {
     await cph.outputEnded;
     // On Windows, for whatever reason, `cp.kill("SIGINT")` does not result in the child's SIGINT handler being called,
     //   but instead the immediate termination of the child.
-    if (process.platform === "win32") {
+    if (isWindows) {
       expect(cph.readOutput()).toStrictEqual(["", ""]);
       spawnSync("docker", ["kill", "hello-goodbye"]);
     } else {
       expect(cph.readOutput()).toStrictEqual([
-        `> docker kill ${killArgs.join(" ")} hello-goodbye`,
-        "hello-goodbye",
+        "goodbye",
+        "Killed container hello-goodbye",
         "",
         ""
       ]);
     }
     await cph.childExited;
-    expect(cp.exitCode).toBeNull();
-    expect(cp.signalCode).toBe("SIGINT");
+    expect(cp.exitCode).toBe(isWindows ? null : 130);
   });
   it("help", async () => {
-    const { stdout, stderr, status, signal } = spawnBinSync(["--help"]);
+    const { stdout, stderr, status } = spawnBinSync(["--help"]);
     expect(stdout.toString("utf8")).toMatchInlineSnapshot(`
       "
       Usage:  docker-run-kill [RUN-KILL OPTIONS] [KILL OPTIONS] [RUN OPTIONS] IMAGE [COMMAND] [ARG...]
@@ -70,7 +76,6 @@ describe("cli", () => {
     `);
     expect(stderr.toString("utf8")).toBe("");
     expect(status).toBe(0);
-    expect(signal).toBeNull();
   });
 });
 
