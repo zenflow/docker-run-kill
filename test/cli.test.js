acd856654a88fc7c8cdc0fb13a61f5ba1d54e2cf
Matthew Francis Brunetti <zenflow87@gmail.com>
2020-09-05 17:06:45 -0400

    Pretty up tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,20 +1,35 @@
-const { spawn, spawnSync } = require("child_process");
-const { join } = require("path");
-const { getChildProcessHelpers } = require("./util/getChildProcessHelpers");
+/*
+  Note: On Windows, for whatever reason, calling `cp.kill("SIGINT")`
+  does not result in the child's SIGINT handler being called,
+  but instead the immediate termination of the child.
+ */
 
 const dockerNodeImage = "node:10.22.0-alpine3.9";
+
+const { spawn, spawnSync } = require("child_process");
+const { join } = require("path");
+const { helpText } = require("../lib/helpText");
+const { getChildProcessHelpers } = require("./util/getChildProcessHelpers");
+
 const isWindows = process.platform === "win32";
 
 describe("cli", () => {
   it("child exit (don't kill container)", async () => {
     // TODO: Why does this test take so long, especially in Github Actions?
     jest.setTimeout(10000);
-    const runArgs = ["--rm", dockerNodeImage, "-e", "console.log('hello')"];
-    const cp = spawnBin(["--name", "hello", ...runArgs]);
+    const script = "console.log('hello')";
+    const cp = spawnBin([
+      "--name",
+      "hello",
+      "--rm",
+      dockerNodeImage,
+      "-e",
+      script
+    ]);
     const cph = getChildProcessHelpers(cp);
     await cph.outputEnded;
     expect(cph.readOutput()).toStrictEqual([
-      `> docker run --name hello ${runArgs.join(" ")}`,
+      `> docker run --name hello --rm ${dockerNodeImage} -e ${script}`,
       "hello",
       "",
       ""
@@ -25,24 +40,24 @@ describe("cli", () => {
   it("signal exit (kill container)", async () => {
     const script =
       "console.log('hello');process.on('SIGTERM',()=>{console.log('goodbye');process.exit();});setInterval(()=>{},1000)";
-    const runArgs = ["--rm", dockerNodeImage, "-e", script];
     const cp = spawnBin([
       "--name",
       "hello-goodbye",
       "--signal",
       "SIGTERM",
-      ...runArgs
+      "--rm",
+      dockerNodeImage,
+      "-e",
+      script
     ]);
     const cph = getChildProcessHelpers(cp);
     await cph.when(line => line === "hello");
     expect(cph.readOutput()).toStrictEqual([
-      `> docker run --name hello-goodbye ${runArgs.join(" ")}`,
+      `> docker run --name hello-goodbye --rm ${dockerNodeImage} -e ${script}`,
       "hello"
     ]);
     cp.kill("SIGINT");
     await cph.outputEnded;
-    // On Windows, for whatever reason, `cp.kill("SIGINT")` does not result in the child's SIGINT handler being called,
-    //   but instead the immediate termination of the child.
     if (isWindows) {
       expect(cph.readOutput()).toStrictEqual(["", ""]);
       spawnSync("docker", ["kill", "hello-goodbye"]);
@@ -59,21 +74,7 @@ describe("cli", () => {
   });
   it("help", async () => {
     const { stdout, stderr, status } = spawnBinSync(["--help"]);
-    expect(stdout.toString("utf8")).toMatchInlineSnapshot(`
-      "
-      Usage:  docker-run-kill [RUN-KILL OPTIONS] [KILL OPTIONS] [RUN OPTIONS] IMAGE [COMMAND] [ARG...]
-
-      Run a command in a new container and kill that container when process is terminated.
-
-      Run-kill Options:
-          --help           Output usage information
-          --name string    Assign a name to the container (default is \\"docker_run_kill_{x}\\" where {x} is a random number)
-
-      Documentation for Kill Options & Run Options can be output with \`docker kill --help\` & \`docker run --help\` respectively.
-
-      *Important* To assign a name to the container, pass \`--name\` as a Run-kill Option, not a Run Option, since the latter won't be recognized.
-      "
-    `);
+    expect(stdout.toString("utf8")).toBe(`${helpText}\n`);
     expect(stderr.toString("utf8")).toBe("");
     expect(status).toBe(0);
   });
